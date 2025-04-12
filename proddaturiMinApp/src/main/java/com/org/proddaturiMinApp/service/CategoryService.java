e6ba9f86bdda7b6617c951bc335a3db81b10ddbe
Thillak <thillakkumar2002@gmail.com>
2025-04-12 13:19:30 +0530

    code fixes

--- proddaturiMinApp/src/main/java/com/org/proddaturiMinApp/service/CategoryService.java
+++ proddaturiMinApp/src/main/java/com/org/proddaturiMinApp/service/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -42,4 +43,8 @@ public class CategoryService implements CategoryServiceInterface {
         categoryRepository.deleteById(id);
         return commonConstants.dataDeleted;
     }
+    public List<Category> allCategories() {
+        return categoryRepository.findAll();
+    }
+
 }
